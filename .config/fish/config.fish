0def633ff87e4b85b6f6fd0b731e68119b4d534c
Kevin Mertens <kevin_mertens@telenet.be>
2025-12-04 09:23:47 +0100

    added gitconfig and configured Eza theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,15 @@ end
 starship init fish | source
 
 enable_transience
+
+alias ls="eza --color=always --group-directories-first --icons --git-ignore --git"
+alias ll="eza --color=always --group-directories-first --icons --header --long --git-ignore --git"
+alias la="eza -a --color=always --group-directories-first --icons --header --long --git-ignore --git"
+alias tree="eza -T -a --color=always --group-directories-first --icons --git-ignore"
+
+alias gs="git status"
+alias gd="git diff"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gu="git pull"
