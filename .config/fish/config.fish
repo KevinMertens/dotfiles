793af51a67af3477e0d88f8bcb022e6d9c4e6470
Kevin Mertens <kevin_mertens@telenet.be>
2025-12-05 16:03:08 +0100

    moved Fish config inside to interactive move + updated Hyprland config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,31 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    function fish_greeting
+        fastfetch
+    end
+
+    starship init fish | source
+    zoxide init fish | source
+
+    enable_transience
+
+    alias cd="z"
+    alias cdi="zi"
+
+    alias ls="eza --color=always --group-directories-first --icons --git-ignore --git"
+    alias ll="eza --color=always --group-directories-first --icons --header --long --git-ignore --git"
+    alias la="eza -a --color=always --group-directories-first --icons --header --long --git-ignore --git"
+    alias tree="eza -T -a --color=always --group-directories-first --icons --git-ignore"
+
+    alias gs="git status"
+    alias gd="git diff"
+    alias ga="git add"
+    alias gc="git commit"
+    alias gp="git push"
+    alias gu="git pull"
+
+    alias tldr="tldr -Len"
+    alias rg="rg --hidden"
+
 end
-
-function fish_greeting
-	fastfetch
-end
-
-starship init fish | source
-
-enable_transience
-
-alias ls="eza --color=always --group-directories-first --icons --git-ignore --git"
-alias ll="eza --color=always --group-directories-first --icons --header --long --git-ignore --git"
-alias la="eza -a --color=always --group-directories-first --icons --header --long --git-ignore --git"
-alias tree="eza -T -a --color=always --group-directories-first --icons --git-ignore"
-
-alias gs="git status"
-alias gd="git diff"
-alias ga="git add"
-alias gc="git commit"
-alias gp="git push"
-alias gu="git pull"
