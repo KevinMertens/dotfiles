dc61178040f761b01308bd7bf03e5667cc9f3720
Kevin Mertens <kevin_mertens@telenet.be>
2025-12-09 19:59:32 +0100

    updated Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,17 +8,22 @@ if status is-interactive
     starship init fish | source
     zoxide init fish --cmd cd | source
 
+    # https://github.com/sharkdp/vivid
+    set -gx LS_COLORS "$(vivid generate catppuccin-mocha)"
+
     enable_transience
 
     # https://github.com/eza-community/eza
     alias ls="eza --color=always --group-directories-first --icons --git-ignore --git"
     alias ll="eza --color=always --group-directories-first --icons --header --long --git-ignore --git"
     alias la="eza -a --color=always --group-directories-first --icons --header --long --git-ignore --git"
+    alias lg="eza -a --color=always --group-directories-first --icons --header --long --git"
     alias tree="eza -T -a --color=always --group-directories-first --icons --git-ignore"
 
+    # https://github.com/sharkdp/fd
+    alias find="fd --hidden --exclude .git --color=always"
+
     alias vim="nvim"
-    alias vi="nvim"
-    alias nano="nvim"
 
     alias gs="git status"
     alias gd="git diff"
