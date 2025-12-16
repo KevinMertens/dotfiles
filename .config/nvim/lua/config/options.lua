9a57ea5819cdbe5091e594d3c634989c2ea16cb8
Kevin Mertens <kevin_mertens@telenet.be>
2025-12-16 19:20:32 +0100

    added Neovim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
+-- https://neovim.io/doc/user/options.html
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
@@ -15,13 +17,13 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 -- TODO : vim.o.showmode = false
 
--- TODO : Sync clipboard between OS and Neovim.
+-- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.schedule(function()
---  vim.o.clipboard = 'unnamedplus'
--- end)
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
 
 -- Enable break indent
 vim.o.breakindent = true
