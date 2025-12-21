3020a92180a68881c6a9c48fef3c314b7150ad6e
Kevin Mertens <kevin_mertens@telenet.be>
2025-12-21 09:59:16 +0100

    added treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "vim",
+      "vimdoc",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { "ruby" },
+    },
+    indent = { enable = false },
+  },
+}
