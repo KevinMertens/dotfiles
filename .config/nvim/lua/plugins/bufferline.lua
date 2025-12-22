3412f1039975ef00157534fac5bbc5422ddda80b
Kevin Mertens <kevin_mertens@telenet.be>
2025-12-22 09:49:06 +0100

    added lualine and bufferline

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+-- https://github.com/akinsho/bufferline.nvim
+
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup()
+  end,
+}
